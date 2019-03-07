09eba4f4c0240a43f316199af0f20b9a0a4f2c50
itggot-linus-ohrn <linus.ohrn@elev.ga.ntig.se>
2019-03-07 15:42:31 +0100

    jwa

---
+++ site/js/main.js
@@ -0,0 +1,8 @@
+function doStuff(event){
+    // let klick = document.querySelector(".sidebar")
+    let shadow = document.querySelector(".shadow")
+    let menu = document.querySelector(".menu")
+    // klick.classList.toggle("sidebar-activate")
+    shadow.classList.toggle("shadow-activate")
+    menu.classList.toggle("menu-activate")
+}
