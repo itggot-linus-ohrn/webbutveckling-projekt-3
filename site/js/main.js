7ae5b305f7f1a9e969d44d8722d98b55285aaacc
itggot-linus-ohrn <linus.ohrn@elev.ga.ntig.se>
2019-03-07 21:56:15 +0100

    blabla

--- site/js/main.js
+++ site/js/main.js
@@ -1,8 +1,14 @@
 function doStuff(event){
-    // let klick = document.querySelector(".sidebar")
+    let klick = document.querySelector("ul")
     let shadow = document.querySelector(".shadow")
     let menu = document.querySelector(".menu")
-    // klick.classList.toggle("sidebar-activate")
+    let box1 = document.querySelector(".box1")
+    let box2 = document.querySelector(".box2")
+    let box3 = document.querySelector(".box3")
+    klick.classList.toggle("ul-activate")
     shadow.classList.toggle("shadow-activate")
     menu.classList.toggle("menu-activate")
+    box1.classList.toggle("box-active")
+    box2.classList.toggle("box-active")
+    box3.classList.toggle("box-active")
 }
