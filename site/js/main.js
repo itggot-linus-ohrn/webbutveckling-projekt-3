3e8983729b194ee73f2911b813c70afc044fe727
itggot-linus-ohrn <linus.ohrn@elev.ga.ntig.se>
2019-03-14 15:46:49 +0100

    did things

--- site/js/main.js
+++ site/js/main.js
@@ -1,8 +1,19 @@
 function doStuff(event){
     let klick = document.querySelector("ul")
     let shadow = document.querySelector(".shadow")
-    let menu = document.querySelector(".menu")
+    let menu = document.querySelectorAll(".menu")
+    let header = document.querySelector(".header")
     klick.classList.toggle("ul-activate")
     shadow.classList.toggle("shadow-activate")
-    menu.classList.toggle("menu-activate")
+    for(i=0; i<menu.length; i++){
+    menu[i].classList.toggle("menu" + i + "-activate")
+
+    if (menu[i].innerHTML == "Menu"){
+        menu[i].innerHTML = "Close"
+    }else{
+        menu[i].innerHTML = "Menu"
+    }
+}
+
+    header.classList.toggle("header-active")
 }
